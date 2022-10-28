b08366eaddbbc527313343846642022a236cbe3e
luksvrd <ljvy9b@gmail.com>
2022-10-27 21:53:14 -0500

    solved 10/27

--- pages/11-light-dark-toggle/script.js
+++ pages/11-light-dark-toggle/script.js
@@ -1,4 +1,10 @@
-// TODO: Select the HTML element
+const { Input } = require("postcss");
 
+// TODO: Select the HTML element
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input");
 // TODO: Add 'dark' to its class list whenever the checkbox is checked
 // HINT: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+Input.addEventListener("change", function () {
+  html.classList.toggle("dark");
+});
--- pages/12-counter/script.js
+++ pages/12-counter/script.js
@@ -1,2 +1,22 @@
 // TODO: Wire up the buttons to update the count.
+const button = document.querySelectorAll("button");
+const count = document.querySelectorAll("#count");
+
+function handleButtonClick(event) {
+  // get the text Content of count - turn it into a number
+  const countValue = Number(count.textContent);
+
+  if (event.target.id === "increment") {
+    count.textContent = countValue + 1;
+  } else if (event.target.id === "decrement") {
+    if (countValue > 0) (
+      count.textContent = countValue - 1;
+    )
+  }
+}
+
+
 // TODO: Don't allow the count to go below 0.
+buttons.forEach((button) function {{
+  button.addEventListener("click, handleCuttonClick")
+}})
