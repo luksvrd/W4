e9b81e90dac89bc4a5fade97f2db1bcfbc082241
luksvrd <ljvy9b@gmail.com>
2022-10-31 19:32:48 -0500

    tip math

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,20 +1,34 @@
 // TODO: Get the form submission as an object
 const form = document.querySelector("form");
+const tipAmtP = document.querySelector("#tipAmt");
+const totalAmtP = document.querySelector("#totalAmt")
 
 // TODO: Write a function to calculate the total amount
 function calcTotalWithTip(total, tipPct) {
-  return total + total * (tipPct / 100);
+  return Number(total) + total * (tipPct / 100);
 }
 
 function handleSubmit(e) {
-  // prevents the browsers default behavior of trying to submit a form and the page reloading.
   e.preventDefault();
 
   const submission = {
     total: e.target.total.value,
     tip: e.target.tip.value,
   };
+
+  const totalWithTip = calcTotalWithTip(submission.total, submission.tip);
+
+  renderTipInfo(totalWithTip, submission.tip);
 }
+
+function renderTipInfo(totalAmt, tipAmt) {
+  tipAmtP.textContent = `Tip Amount: $${tipAmt}`;
+  totalAmtP.textContent = `Total Amount: $${totalAmt}`;
+}
+const totalWithTip = calcTotalWithTip(submission.total, submisison.tip);
+renderTipinfo(totalWithTip, submission.tip);
+
+function renderTipinfo
 
 form.addEventListener("submit", handleSubmit);
 
