27b4592bea616c72d23c2cc170b7d142bdb98f52
luksvrd <ljvy9b@gmail.com>
2022-10-31 19:10:23 -0500

    function to calculate the total amount

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,6 +1,11 @@
 // TODO: Get the form submission as an object
 const form = document.querySelector("form");
 
+// TODO: Write a function to calculate the total amount
+function calcTotalWithTip(total, tipPct) {
+  return total + total * (tipPct / 100);
+}
+
 function handleSubmit(e) {
   // prevents the browsers default behavior of trying to submit a form and the page reloading.
   e.preventDefault();
@@ -13,16 +18,4 @@ function handleSubmit(e) {
 
 form.addEventListener("submit", handleSubmit);
 
-// TODO: Write a function to calculate the total amount
-// const tipSum = (arr => {
-//   let sum = 0;
-//   for (t = 0; t < arr.length; t++)
-//     {
-//       if (typeof arr [t] == "number")
-//       {
-//         sum = sum + arr [t];
-//       }
-//     }
-//     return sum;
-// }
 // TODO: Output the information showing voff, the tip amount and the total in 'p' tags
